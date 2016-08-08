7a07f31618b87e127123d4c84e1c624eff8b160a
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-08 13:33:12 +0200

    Added controller to register and get user

---
+++ WebApiOwinIdentityOAuthJwtSample/WebApiOwinIdentityOAuthJwtSample/Controllers/IdentityController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApiOwinIdentityOAuthJwtSample.Models;
+
+namespace WebApiOwinIdentityOAuthJwtSample.Controllers
+{
+    [RoutePrefix("identity")]
+    public class IdentityController : ApiController
+    {
+        private const string GetUserRouteName = "GetUser";
+        private UserManager<IdentityUser> UserManager => Request.GetOwinContext().GetUserManager<UserManager<IdentityUser>>();
+
+        [Authorize]
+        [HttpGet]
+        [Route("{userId}", Name = GetUserRouteName)]
+        public async Task<IHttpActionResult> Get(string userId)
+        {
+            var user = await UserManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IHttpActionResult> Register(Registration model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (await UserManager.FindByNameAsync(model.UserName) != null)
+            {
+                return Conflict();
+            }
+
+            var user = new IdentityUser(model.UserName);
+            var result = await UserManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtRoute(GetUserRouteName, new { userId = user.Id }, user);
+        }
+    }
+}
---
+++ WebApiOwinIdentityOAuthJwtSample/WebApiOwinIdentityOAuthJwtSample/Models/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiOwinIdentityOAuthJwtSample.Models
+{
+    public class Registration
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(Int32.MaxValue, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string PasswordConfirmation{ get; set; }
+    }
+}
