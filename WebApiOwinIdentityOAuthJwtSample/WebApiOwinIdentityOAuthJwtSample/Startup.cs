93fa77e699ecb4b003bc09289235ec59839827b6
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-08 11:12:53 +0200

    Added ASP.NET WebAPI & OWIN configuration

---
+++ WebApiOwinIdentityOAuthJwtSample/WebApiOwinIdentityOAuthJwtSample/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartup(typeof(WebApiOwinIdentityOAuthJwtSample.Startup))]
+
+namespace WebApiOwinIdentityOAuthJwtSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            ConfigureWebApi(app);
+        }
+    }
+}
