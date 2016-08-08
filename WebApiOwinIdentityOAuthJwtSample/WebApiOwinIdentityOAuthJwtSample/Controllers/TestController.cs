5ac84d28288081e8ffa24922b5b2168a5d2758a1
Fabian Vilers <fabian.vilers@dev-one.com>
2016-08-08 11:14:01 +0200

    Added a test controller to ensure that WebAPI is working

---
+++ WebApiOwinIdentityOAuthJwtSample/WebApiOwinIdentityOAuthJwtSample/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace WebApiOwinIdentityOAuthJwtSample.Controllers
+{
+    [RoutePrefix("test")]
+    public class TestController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
